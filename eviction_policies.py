10f8d5dda882c1b237958ce83a6523624fe87570
pablopupo <145598901+pablopupo@users.noreply.github.com>
2026-03-03 14:28:21 -0500

    Fifo implementation completed

---
+++ eviction_policies.py
@@ -0,0 +1,62 @@
+import sys
+from collections import deque, OrderedDict
+
+
+def fifo(capacity: int, requests: list[int]) -> int:
+    """Returns the number of cache misses under First-In, First-Out policy"""
+    cache_misses = 0
+    fifo_queue = deque()
+    cache_set = set()
+    for request in requests:
+        if request in cache_set:
+            continue
+
+        if len(fifo_queue) == capacity:
+            evicted = fifo_queue.popleft()
+            cache_set.remove(evicted)
+
+        fifo_queue.append(request)
+        cache_set.add(request)
+        cache_misses += 1
+    return cache_misses
+
+
+
+
+def main():
+    """Usage: python eviction_policies.py <input_file>"""
+
+    # Ex. Input:
+    # 3 12
+    # 1 2 3 4 1 3 2 1 2 3 4 1
+    if len(sys.argv) < 2:
+        print("Usage: python eviction_policies.py <file_input>")
+
+    file_path = sys.argv[1]
+
+    with open(file_path, 'r') as file:
+        header = file.readline().split()
+        if not header:
+            print("Error: input file must be non-empty")
+            return
+
+        k, _ = map(int, header)
+
+        line = file.readline()
+        data = (list(map(int, line.split())))
+
+    fifo_res = fifo(k, data)
+    lru_res = lru(k, data)
+
+    print("FIFO policy cache misses: ", fifo_res)
+    # print("LRU policy cache misses: ", lru_res)
+
+    # Output:
+    # FIFO  : <number_of_misses>
+    # LRU   : <number_of_misses>
+    # OPTFF : <number_of_misses>
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
